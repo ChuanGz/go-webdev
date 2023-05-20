695f5cc57a40281ce08be6f888f59e923ff9704d
Chuangz <thanhnhan.lionus@gmail.com>
2023-05-20 10:33:11 +0700

    int and float32 cast

--- Go/functions.go
+++ Go/functions.go
@@ -1,13 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func add(x int, y int, z int) int {
-	return x + y + z
+func add(x, y, z int, f1, f2 float32) float32 {
+	return float32(x+y+z) + f1 + f2
 }
 
 func main() {
-	fmt.Println(add(2, 2, 6))
+	fmt.Println(add(2, 2, 6, 3.2, 0.7))
 }
