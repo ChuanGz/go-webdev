36a51ff97cf1e2fa735d099787455e4f37959bb0
Chuangz <thanhnhan.lionus@gmail.com>
2023-05-20 11:41:47 +0700

    pitago triangle

--- Go/variable-initializer.go
+++ Go/variable-initializer.go
@@ -9,7 +9,7 @@ var globCheck bool = true
 var globAuthName string = "ChuanGz"
 var globDate time.Time = time.Now()
 
-func main() {
+func main4() {
 	var pi float64 = 2.33445566
 	fmt.Println("function level")
 	fmt.Println(pi)
