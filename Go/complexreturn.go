4657d57d1939557c1f1016dbacef99942141c139
Chuangz <thanhnhan.lionus@gmail.com>
2023-05-20 11:18:15 +0700

    named result for find element of perfect number problem

--- Go/complexreturn.go
+++ Go/complexreturn.go
@@ -9,7 +9,7 @@ func multipleSquare(list [4]int) (float64, float64, float64, float64) {
 	return math.Sqrt(float64(list[0])), math.Sqrt(float64(list[1])), math.Sqrt(float64(list[2])), math.Sqrt(float64(list[3]))
 }
 
-func main() {
+func main2() {
 	var myinput [4]int
 	myinput = [4]int{10, 20, 30, 40}
 	fmt.Println(multipleSquare(myinput))
---
+++ Go/namedreturn.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Inductive(perfected int) (x, y, z int) {
+	for i := 1; i < perfected; i++ {
+		if perfected%i == 0 {
+			x = i
+		}
+	}
+	for i := 1; i < perfected; i++ {
+		if perfected%i == 0 && x != i {
+			z = i
+		}
+	}
+	for j := perfected; j > 0; j-- {
+		if perfected%j == 0 {
+			y = j
+		}
+	}
+	return
+}
+
+func main() {
+	fmt.Println(Inductive(6))
+	fmt.Println(Inductive(36))
+	fmt.Println(Inductive(16))
+	fmt.Println(Inductive(1388))
+}
