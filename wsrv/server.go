dcc2bd56b98abd7fd12f8f6acea244fff2b184f2
Chuangz <thanhnhan.lionus@gmail.com>
2023-06-04 16:35:26 +0700

    header

--- wsrv/server.go
+++ wsrv/server.go
@@ -10,9 +10,10 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "POST" {
+			w.Header().Add("Server", "Local")
 			fmt.Fprintf(w, "Hello, %q, This is POST with body: %q", "ChuanGz", r.Body)
 		} else {
-
+			w.Header().Add("Method", r.Method)
 			fmt.Fprintf(w, "Hello, %q, This is %q", "ChuanGz", r.Method)
 		}
 	})
